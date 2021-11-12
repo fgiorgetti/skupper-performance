9f5a9f9b9b80b4829fe2bb286f4562f3091a8a63
Fernando Giorgetti <fgiorget@redhat.com>
2021-11-12 12:14:37 -0300

    Updated archive-results to keep just the tar ball

--- onpremise-cloud/archive-results.sh
+++ onpremise-cloud/archive-results.sh
@@ -3,9 +3,10 @@
 SUFFIX=""
 [[ -n "${1}" ]] && SUFFIX="-${1}"
 dir="`date +%Y%m%d`${SUFFIX}"
+[[ -z "${dir}" ]] && echo "unable to generate destination directory" && exit 1
 bkpdir="archive/${dir}"
 [[ -d ${bkpdir} ]] && echo "${bkpdir} already exists - use a different suffix" && exit 1
 mkdir -p ${bkpdir}
 cp charts.html ${bkpdir}
 mv results/ ${bkpdir}
-( cd archive; tar zcvf ${dir}.tar.gz ${dir} )
+( cd archive; tar zcvf ${dir}.tar.gz ${dir} && rm -rf ${dir} )
